bfca5d8e18f6f5f4be179c0fa4b2155fc1330d73
Kamil Dzierzawski <kamildzierzawski@gmail.com>
2024-02-20 00:47:44 +0100

    Initial commit

---
+++ src/main/java/com/project/sca/ChatController.java
@@ -0,0 +1,17 @@
+package com.project.sca;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public MessageDTO sendMessage(
+            @Payload MessageDTO chatMessage
+    ) {
+        return chatMessage;
+    }
+}
---
+++ src/test/java/com/project/sca/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.project.sca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
